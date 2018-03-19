b80d404d587278ee696070af41010a427d8369f2
ajgan <ajgan@cin.ufpe.br>
2018-03-19 16:19:28 -0300

    Ajeitando crawler

--- instagramcrawler.py
+++ instagramcrawler.py
@@ -270,9 +270,7 @@ class InstagramCrawler(object):
         if 'm ' in str(follow_ele.text):
             valor = valor * 1000000
         # open desired list
-        if (valor > 1000):
-            valor = 900
-        follow_ele.click()
+        
 
         title_ele = WebDriverWait(self._driver, 10).until(
             EC.presence_of_element_located(
